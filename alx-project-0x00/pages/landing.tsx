6b57747908e22921e59cd47f4db500ea9d5c1eee
khembo <khembo.douglas.juma@gmail.com>
2025-05-28 00:22:27 +0300

    modify

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -17,16 +17,19 @@ const Landing: React.FC = () => {
         <Button title="Small - Rounded Small" styles="text-sm rounded-sm" />
         <Button title="Small - Rounded Medium" styles="text-sm rounded-md" />
         <Button title="Small - Rounded Full" styles="text-sm rounded-full" />
+        <Button title="Small - Rounded LG" styles="text-sm rounded-lg" />
 
         {/* Medium Buttons */}
         <Button title="Medium - Rounded Small" styles="text-base rounded-sm" />
         <Button title="Medium - Rounded Medium" styles="text-base rounded-md" />
         <Button title="Medium - Rounded Full" styles="text-base rounded-full" />
+        <Button title="Medium - Rounded LG" styles="text-base rounded-lg" />
 
         {/* Large Buttons */}
         <Button title="Large - Rounded Small" styles="text-lg rounded-sm" />
         <Button title="Large - Rounded Medium" styles="text-lg rounded-md" />
         <Button title="Large - Rounded Full" styles="text-lg rounded-full" />
+        <Button title="Large - Rounded LG" styles="text-lg rounded-lg" />
       </div>
     </div>
   );
